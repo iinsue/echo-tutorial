1f936757a702d4a153babe96059ea63052f6c857
insu <hinsu93@gmail.com>
2025-10-09 22:36:16 +0900

    #2-7 사용자 추가 api 연동 테스트

--- packages/backend/convex/users.ts
+++ packages/backend/convex/users.ts
@@ -1,4 +1,4 @@
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 export const getMany = query({
   args: {},
@@ -6,5 +6,16 @@ export const getMany = query({
     const users = await context.db.query("users").collect();
 
     return users;
+  },
+});
+
+export const add = mutation({
+  args: {},
+  handler: async (context) => {
+    const userId = await context.db.insert("users", {
+      name: "INSU",
+    });
+
+    return userId;
   },
 });
