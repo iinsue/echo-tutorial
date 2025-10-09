2bf13ca727da7db2606cbbde579604d7f71e8800
insu <hinsu93@gmail.com>
2025-10-09 22:25:12 +0900

    #2-6 widget 프로젝트 api 연동 테스트

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -7,9 +7,11 @@ export default function Page() {
   const users = useQuery(api.users.getMany);
 
   return (
-    <div className="flex items-center justify-center min-h-svh">
+    <div className="flex flex-col items-center justify-center min-h-svh">
       <p>apps/web</p>
-      {JSON.stringify(users)}
+      <div className="max-w-sm w-full mx-auto">
+        {JSON.stringify(users, null, 2)}
+      </div>
     </div>
   );
 }
