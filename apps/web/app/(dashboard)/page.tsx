af1e30b64ac5379f5318775917f6850c7b87c6e5
insu <hinsu93@gmail.com>
2025-10-15 23:20:27 +0900

    #5-1 Sentry Setup

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 
+import { useMutation } from "convex/react";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
-import { useMutation, useQuery } from "convex/react";
 
 import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
-  const users = useQuery(api.users.getMany);
   const addUser = useMutation(api.users.add);
 
   return (
@@ -16,9 +15,6 @@ export default function Page() {
       <UserButton />
       <OrganizationSwitcher hidePersonal={true} />
       <Button onClick={() => addUser()}>사용자추가</Button>
-      <div className="max-w-sm w-full mx-auto">
-        {JSON.stringify(users, null, 2)}
-      </div>
     </div>
   );
 }
---
+++ apps/web/sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://6b7a72edb8d2dc96867106968368a392@o4507417398345728.ingest.us.sentry.io/4510193836097536",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
