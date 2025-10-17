74397dd55eb40a5d8f720d4985c332ad90ce311a
insu <hinsu93@gmail.com>
2025-10-18 00:22:57 +0900

    #7-2 Add Sidebar

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -1,16 +1,11 @@
-import { AuthGuard } from "@/modules/auth/ui/components/auth-guard";
-import { OrganizationGuard } from "@/modules/auth/ui/components/organization-guard";
+import { DashboardLayout } from "@/modules/dashboard/ui/layouts/dashboard-layout";
 
 interface Props {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: Props) => {
-  return (
-    <AuthGuard>
-      <OrganizationGuard>{children}</OrganizationGuard>
-    </AuthGuard>
-  );
+  return <DashboardLayout>{children}</DashboardLayout>;
 };
 
 export default Layout;
