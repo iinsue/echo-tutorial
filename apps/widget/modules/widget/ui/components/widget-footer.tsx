3d525e77ab8e070398d6aeeda6a75b0d812718b5
insu <hinsu93@gmail.com>
2025-10-18 21:08:32 +0900

    #9-1 Widget Layout

---
+++ apps/widget/modules/widget/ui/components/widget-footer.tsx
@@ -0,0 +1,37 @@
+import { HomeIcon, InboxIcon } from "lucide-react";
+
+import { cn } from "@workspace/ui/lib/utils";
+import { Button } from "@workspace/ui/components/button";
+
+export const WidgetFooter = () => {
+  const screen = "selection";
+
+  return (
+    <footer className="flex items-center justify-between border-t bg-background">
+      <Button
+        size="icon"
+        variant="ghost"
+        className="h-14 flex-1 rounded-none"
+        onClick={() => {}}
+      >
+        <HomeIcon
+          className={cn("size-5", screen === "selection" && "text-primary")}
+        />
+      </Button>
+
+      <Button
+        size="icon"
+        variant="ghost"
+        className="h-14 flex-1 rounded-none"
+        onClick={() => {}}
+      >
+        <InboxIcon
+          className={cn(
+            "size-5"
+            //screen === "inbox" && "text-primary"
+          )}
+        />
+      </Button>
+    </footer>
+  );
+};
