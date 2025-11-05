a80cc7ce58849e9da2fc72155635b8392b98e4a3
insu <hinsu93@gmail.com>
2025-11-05 21:21:40 +0900

    #17 Dashboard Inbox

---
+++ apps/web/modules/dashboard/ui/layouts/conversations-layout.tsx
@@ -0,0 +1,26 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@workspace/ui/components/resizable";
+import { ConversationsPanel } from "../components/conversations-panel";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const ConversationsLayout = ({ children }: Props) => {
+  return (
+    <ResizablePanelGroup className="h-full flex-1" direction="horizontal">
+      <ResizablePanel defaultSize={30} maxSize={30} minSize={20}>
+        <ConversationsPanel />
+      </ResizablePanel>
+
+      <ResizableHandle />
+
+      <ResizablePanel className="h-full" defaultSize={70}>
+        {children}
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  );
+};
---
+++ packages/backend/convex/private/conversations.ts
@@ -0,0 +1,175 @@
+import { ConvexError, v } from "convex/values";
+import { MessageDoc, saveMessage } from "@convex-dev/agent";
+import { paginationOptsValidator, PaginationResult } from "convex/server";
+
+import { components } from "../_generated/api";
+import { mutation, query } from "../_generated/server";
+
+import { supportAgent } from "../system/ai/agents/supportAgent";
+import { Doc } from "../_generated/dataModel";
+
+export const getMany = query({
+  args: {
+    paginationOpts: paginationOptsValidator,
+    status: v.optional(
+      v.union(
+        v.literal("unresolved"),
+        v.literal("escalated"),
+        v.literal("resolved")
+      )
+    ),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (identity === null) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Identity not found",
+      });
+    }
+
+    const orgId = identity.orgId as string;
+
+    if (!orgId) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Organization not found",
+      });
+    }
+
+    let conversations: PaginationResult<Doc<"conversations">>;
+
+    if (args.status) {
+      conversations = await ctx.db
+        .query("conversations")
+        .withIndex("by_status_and_organization_id", (q) =>
+          q
+            .eq("status", args.status as Doc<"conversations">["status"])
+            .eq("organizationId", orgId)
+        )
+        .order("desc")
+        .paginate(args.paginationOpts);
+    } else {
+      conversations = await ctx.db
+        .query("conversations")
+        .withIndex("by_organization_id", (q) => q.eq("organizationId", orgId))
+        .order("desc")
+        .paginate(args.paginationOpts);
+    }
+
+    const conversationsWithAddtionalData = await Promise.all(
+      conversations.page.map(async (conversation) => {
+        let lastMessage: MessageDoc | null = null;
+
+        const contactSession = await ctx.db.get(conversation.contactSessionId);
+
+        if (!contactSession) {
+          return null;
+        }
+
+        const messages = await supportAgent.listMessages(ctx, {
+          threadId: conversation.threadId,
+          paginationOpts: { numItems: 1, cursor: null },
+        });
+
+        if (messages.page.length > 0) {
+          lastMessage = messages.page[0] ?? null;
+        }
+
+        return {
+          ...conversation,
+          lastMessage,
+          contactSession,
+        };
+      })
+    );
+
+    const validConversations = conversationsWithAddtionalData.filter(
+      (conv): conv is NonNullable<typeof conv> => conv !== null
+    );
+
+    return {
+      ...conversations,
+      page: validConversations,
+    };
+  },
+});
+
+export const getOne = query({
+  args: {
+    conversationId: v.id("conversations"),
+    contactSessionId: v.id("contactSessions"),
+  },
+  handler: async (ctx, args) => {
+    const session = await ctx.db.get(args.contactSessionId);
+
+    if (!session || session.expiresAt < Date.now()) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Invalid session",
+      });
+    }
+
+    const conversation = await ctx.db.get(args.conversationId);
+
+    if (!conversation) {
+      throw new ConvexError({
+        code: "NOT_FOUND",
+        message: "Conversation not found",
+      });
+    }
+
+    if (conversation.contactSessionId !== session._id) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Incorrect session",
+      });
+    }
+
+    return {
+      _id: conversation._id,
+      status: conversation.status,
+      threadId: conversation.threadId,
+    };
+  },
+});
+
+export const create = mutation({
+  args: {
+    organizationId: v.string(),
+    contactSessionId: v.id("contactSessions"),
+  },
+  handler: async (ctx, args) => {
+    const session = await ctx.db.get(args.contactSessionId);
+
+    if (!session || session.expiresAt < Date.now()) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Invalid session",
+      });
+    }
+
+    const { threadId } = await supportAgent.createThread(ctx, {
+      userId: args.organizationId,
+    });
+
+    await saveMessage(ctx, components.agent, {
+      threadId,
+      message: {
+        role: "assistant",
+        // TODO: Later modify to widget settings' initial message
+        content: "Hello, how can I help you today?",
+      },
+    });
+
+    const conversationId = await ctx.db.insert("conversations", {
+      contactSessionId: session._id,
+      status: "unresolved",
+      organizationId: args.organizationId,
+      threadId,
+    });
+
+    return conversationId;
+  },
+});
