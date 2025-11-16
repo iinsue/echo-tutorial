b1bb1f26b81a27191699a2f9816ce8f42da6932e
insu <hinsu93@gmail.com>
2025-11-17 01:39:03 +0900

    #31-1 add UI protection & billing

--- apps/web/app/(dashboard)/files/page.tsx
+++ apps/web/app/(dashboard)/files/page.tsx
@@ -1,7 +1,22 @@
+import { Protect } from "@clerk/nextjs";
+
 import { FilesView } from "@/modules/files/ui/views/files-view";
+import { PremiumFeatureOverlay } from "@/modules/billing/ui/components/premium-feature-overlay";
 
 const Page = () => {
-  return <FilesView />;
+  return (
+    // key에 있는 값을 plan에 넣어야 합니다.
+    <Protect
+      condition={(has) => has({ plan: "pro" })}
+      fallback={
+        <PremiumFeatureOverlay>
+          <FilesView />
+        </PremiumFeatureOverlay>
+      }
+    >
+      <FilesView />
+    </Protect>
+  );
 };
 
 export default Page;
